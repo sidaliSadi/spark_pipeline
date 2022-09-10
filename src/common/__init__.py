5773f9764cd99c22d71dec6f15f960c96d31873b
sidaliSadi <sidalisadi230@gmail.com>
2022-09-10 15:42:28 +0200

    spark initialisation

---
+++ src/common/__init__.py
@@ -0,0 +1,3 @@
+from .init_spark import init_spark
+
+__all__ = ["init_spark"]
---
+++ src/common/init_spark.py
@@ -0,0 +1,27 @@
+
+from pyspark.sql import SparkSession
+
+def init_spark(
+    job_name,
+    driver_memory=70,
+    cpu_number="*",
+    spark_partitions=200,
+    enable_pyarrow=True,
+):
+    spark_config = (
+        SparkSession.builder.appName(job_name)
+        .config("spark.master", f"local[{cpu_number}]")
+        .config("spark.driver.memory", f"{driver_memory}g")
+        .config("spark.driver.maxResultSize", "10g")
+        .config("spark.sql.shuffle.partitions", spark_partitions)
+        .config("spark.sql.cbo.enabled", True)
+        .config("spark.sql.adaptive.enabled", True)
+        .config("spark.sql.constraintPropagation.enabled", True)
+        .config("spark.sql.execution.arrow.pyspark.enabled", enable_pyarrow)
+        .config("spark.sql.legacy.timeParserPolicy", "CORRECTED")
+        .config(
+            "spark.driver.extraJavaOptions",
+            "-XX:+UseG1GC -Dio.netty.tryReflectionSetAccessible=true",
+        )
+    )
+    return spark_config.getOrCreate()
